f2635ca122032733ec699119846183faf97cc809
Shinji Nakano <127798992+Vharkus@users.noreply.github.com>
2023-03-21 12:05:34 -0300

    Update megaman.cpp

--- megaman.cpp
+++ megaman.cpp
@@ -1,21 +1,25 @@
-// Online C++ compiler to run C++ program online
-#include <iostream>
+#include <iostring>
 #include <string>
 using namespace std;
 int main() {
+    //Declaração das diferentes variaveis.
     int life;
     int energy;
     bool status;
     string character;
-    
+    //Definição dos valores das variaveis.
     life = 5;
     energy = 99.99f;
     status = true;
     character = "Mega Man";
     
+    //Irá mostrar o valor definido na variável.
     cout << life << endl;
+    //Vai mostrar o valor definido na variável.
     cout << energy << endl;
+    //Irá mostrar o número 1 pois "true" vale 1, se fosse "false" iria mostrar 0.
     cout << status << endl;
+    //Vai mostrar o nome, pois definimos lá atrás com ""
     cout << character << endl;
     return 0;
 }
