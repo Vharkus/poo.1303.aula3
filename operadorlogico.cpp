715fb885cabfb64c41687abd40356d6d8ab99567
Shinji Nakano <127798992+Vharkus@users.noreply.github.com>
2023-03-21 11:35:45 -0300

    Update operadorlogico.cpp

--- operadorlogico.cpp
+++ operadorlogico.cpp
@@ -2,23 +2,26 @@
 using namespace std;
 
 int main () {
+    //Declaração e definição das variaveis. Como tem o sinal de igual os valores serão fixos.
     int a = 5;
     int b = 10;
     int c = 15;
     
-    //Exemplo de uso do operador AND (&&)
+    //Foi criada uma condição onde é necessário que A seja menor que B E(&&) que B seja menor que C.
     if (a < b && b < c) {
-        //O operador lógico && está informando 
+        //Cumprida a condição usando o operador a interface será mostrada.
         cout << "A ordem é: a, b, c" << endl;
     }
     
-    //Exemplo do uso do operador OR (||)
+    //Condição que aceita uma das respostas: Se A for maior que B OU(||) B for maior que C.
     if (a > b || b > c) {
+        //Interface refente a condição em questão. Ela surge quando a condição for cumprida.
         cout << "Pelo menos uma das condições é verdadeira" << endl;
     }
     
-    //Exemplo do uso do operador NOT (!)
+    //Operador lógico de negação. Se as variaveis NÃO(!) forem igual ele será ativado.
     if (!(a == b))  {
+        //Interface referente a not que comprova que as variaveis não são iguais. 
         cout << "A é diferente de B" << endl;
     }
     return 0;
