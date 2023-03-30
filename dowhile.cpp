b972bf22fd78b73bc8b9f7b8352c765cb0473a24
Shinji Nakano <127798992+Vharkus@users.noreply.github.com>
2023-03-29 22:59:25 -0300

    Update

--- dowhile.cpp
+++ dowhile.cpp
@@ -1,13 +1,20 @@
-// Online C++ compiler to run C++ program online
+//Biblioteca padrão.
 #include <iostream>
+//Evita a repetição do "std::".
 using namespace std;
+//Programa principal.
 int main() {
+    //Declaração da variável.
     int num;
-    
+    //Tudo dentro do "do" irá acontecer pelo menos uma vez. Se a condição for verdadeira o programa ficará repetindo.
     do{
+        //Interface que pede o número.
         cout << "Digite um número entre 1 e 10: ";
+        //Lê e armazena o número o valor dentro da variável.
         cin >> num;
+        //While irá analisar e ver se a condição foi cumprida ou não.
     } while (num < 1 || num > 10);
+    //Interface que mostra ao usuário o número digitado.
     cout<<"Você digitou o número: " << num << ".";
     return 0;
 }
